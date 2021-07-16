e0be3badb170a321e01f877edc2e4c2d378e9ee8
NV462-ThangVH <thang.vu.hong@nextop.asia>
2021-07-16 11:11:45 +0700

    add test batch

--- src/main/java/com/example/demo_book_club/model/Book.java
+++ src/main/java/com/example/demo_book_club/model/Book.java
@@ -16,8 +16,9 @@ public class Book {
     // - Book (id, name, author, category, price, status)
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "book_id_gen")
-    @SequenceGenerator(name = "book_id_gen", sequenceName = "book_id_gen", allocationSize = 1)
+//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "book_id_gen")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+//    @SequenceGenerator(name = "book_id_gen", sequenceName = "book_id_gen", allocationSize = 1)
     private Integer id;
 
     @Column(name = "name", nullable = false, updatable = false)
